e6818e251a402bcfa6c75f2161055dfd74c19760
moonlightttt <595241427@qq.com>
2022-03-08 17:05:43 +0800

    配置路径

--- JsonGenerator/Program.cs
+++ JsonGenerator/Program.cs
@@ -6,7 +6,8 @@ using System.IO;
 using JsonGenerator.Helper;
 using Microsoft.Extensions.Configuration;
 
-var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+string currentDirectory = Path.GetDirectoryName(typeof(GeneratorHelper).Assembly.Location);
+var configuration = new ConfigurationBuilder().SetBasePath(currentDirectory)
     .AddJsonFile("appsettings.json").Build();
 var cleanerConfig = configuration.GetSection("CleanerConfig").Get<List<GeneratorConfig>>();
 using (var cleaner = new GeneratorHelper(cleanerConfig))
